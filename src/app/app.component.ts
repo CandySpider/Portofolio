b144ed61996f7560f46e3904cc21dba69399ded3
Ruben Pirvu <ruben.matia@yahoo.com>
2024-11-01 16:33:59 +0200

    Created required components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TabsBarComponent } from './tabs-bar/tabs-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { TitleDescriptionComponent } from './title-description/title-description.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { ActionsBarComponent } from './actions-bar/actions-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabsBarComponent, AboutMeComponent, TitleDescriptionComponent, PortfolioComponent, ResumeComponent, ContactComponent, ActionsBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
